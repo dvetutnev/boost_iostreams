fa0a147d717127c09d5324ab9cf9688ba7629227
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-09-19 04:09:37 +0300

    s

--- simple.cpp
+++ simple.cpp
@@ -2,6 +2,8 @@
 
 #include <boost/iostreams/device/array.hpp>
 #include <boost/iostreams/stream.hpp>
+#include <algorithm>
+#include <memory>
 
 std::string read_stream(std::istream& strm) {
     std::string ret;
@@ -23,6 +25,20 @@ TEST(Fake, convertible_2_std_istream)
 {
     const std::string input = "sgr\ng\tanm;lj;zjgv;es\\eeebbvthj";
     ArraySource arr{input.data(), input.size()};
+    StreamArray strm{arr};
+    std::string result = read_stream(strm);
+    ASSERT_EQ(result, input);
+}
+
+TEST(Fake, copy_input)
+{
+    const char* input = "sgr\ng\tanm;lj;zjgv;es\\eeebbvthj";
+    const std::size_t count = sizeof(input);
+    auto data = std::make_unique<char[]>(count);
+    std::copy_n(input, count, data.get());
+    ASSERT_EQ(std::string{data.get()}, input);
+    ArraySource arr{data.get(), count - 1};
+    //data.reset();
     StreamArray strm{arr};
     std::string result = read_stream(strm);
     ASSERT_EQ(result, input);
